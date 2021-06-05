18e2aec5c117aec11611b42a33177afbcc44004b
jiangshaoyue <jiangshaoyue@meituan.com>
2021-06-05 22:27:55 +0800

    1

--- base/src/main/java/com/mid/base/threadModule/ThreadPool/ThreadPoolFutureTaskTest.java
+++ base/src/main/java/com/mid/base/threadModule/ThreadPool/ThreadPoolFutureTaskTest.java
@@ -1,16 +1,17 @@
-package com.mid.base.threadModule.Thread;
+package com.mid.base.threadModule.ThreadPool;
 
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.*;
 
-public class CallableFutureTaskTest {
+public class ThreadPoolFutureTaskTest {
 
-    static class Task1 implements Callable {
+    public static void main(String[] args) {
+        ThreadPoolExecutor executorService = new ThreadPoolExecutor(2, 2,
+                30, TimeUnit.SECONDS, new ArrayBlockingQueue<>(10));
 
-        @Override
-        public Object call() throws Exception {
+        //实现为FutureTask
+        Future future = executorService.submit(() -> {
             System.out.println("Task1 call() 开始执行--");
             Thread.sleep(10000);
             if(1==1){
@@ -18,12 +19,9 @@ public class CallableFutureTaskTest {
             }
             System.out.println("Task1 call() 结束执行--");
             return "success";
-        }
-    }
-    static class Task2 implements Callable{
+        });
 
-        @Override
-        public Object call() throws Exception {
+        Future future2 = executorService.submit(() -> {
             System.out.println("Task2 call() 开始执行--");
             Thread.sleep(2000);
             if(1==1){
@@ -31,43 +29,46 @@ public class CallableFutureTaskTest {
             }
             System.out.println("Task2 call() 结束执行--");
             return "success";
+        });
+
+        /*List<Future> futureTaskList = new LinkedList<>();
+        futureTaskList.add(future);
+        futureTaskList.add(future2);
+        ThreadPoolFutureTaskTest.getResulthadler(futureTaskList);
+        System.out.println("跳出循环");
+        executorService.shutdown();
+        Thread.currentThread().interrupt();*/
+
+
+
+        try {
+            String result = (String)future.get();
+            String result2 = (String)future2.get();
+            System.out.println("----");
+        } catch (Exception e) {
+            System.out.println("future.get Exception");
+            e.printStackTrace();
         }
     }
 
-
-    public static void main(String[] args) {
-        Task1 task1 = new Task1();
-
-        FutureTask future = new FutureTask(task1);
-        Task2 task2 = new Task2();
-        FutureTask future2 = new FutureTask(task2);
-
-        List<FutureTask> futureTaskList = new LinkedList<>();
-        futureTaskList.add(future);
-        futureTaskList.add(future2);
-
+    private static void getResulthadler(List<Future> futureTaskList){
         /**
          * DiscardPolicy：丢弃任务，但是不抛出异常
          * DiscardOldestPolicy：丢弃队列最前面的任务，然后重新提交被拒绝的任务
-         * AbortPolicy:丢弃任务并抛出RejectedExecutionException异常。
+         * AbortPolicy:丢弃任务并抛出RejectedExecutionException异常。   是否会抛到外层？？？
          * CallerRunsPolicy：由调用线程处理该任务,既独自创建线程不走线程池
          *
          * 使用线程池要注意：线程池使用FutureTask的时候拒绝策略设置为DiscardOldestPolicy或AbortPolicy
          * 如果线程被线程只决绝则get()将永远被阻塞  可用超时get(long time)
          */
-        ExecutorService executor = Executors.newCachedThreadPool();
-        for(FutureTask futureTask : futureTaskList){
-            executor.execute(futureTask);
-        }
-
         boolean br = true;
         while(br) {
             if(futureTaskList.size() == 0){
                 br = false;
             }
-            Iterator<FutureTask> it = futureTaskList.iterator();
+            Iterator<Future> it = futureTaskList.iterator();
             while (it.hasNext()) {
-                FutureTask futureTask = it.next();
+                Future futureTask = it.next();
                 if (futureTask.isDone()) {
                     try {
                         //调用get才会获取抛出的异常
@@ -94,8 +95,5 @@ public class CallableFutureTaskTest {
                 }
             }
         }
-        System.out.println("跳出循环");
-        executor.shutdown();
-        Thread.currentThread().interrupt();
     }
 }
