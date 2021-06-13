32f366dee37bd4ceca98abaeb3b578b73eca0b2b
jiangshaoyue <jiangshaoyue@meituan.com>
2021-06-13 22:56:01 +0800

    1

--- base/src/main/java/com/mid/base/threadModule/Lock/ArrayBlockingQueueTest.java
+++ base/src/main/java/com/mid/base/threadModule/Lock/ArrayBlockingQueueTest.java
@@ -1,12 +1,22 @@
 package com.mid.base.threadModule.Lock;
 
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
 
 public class ArrayBlockingQueueTest {
 
     public static void main(String[] args) throws InterruptedException {
-        ArrayBlockingQueue queue = new ArrayBlockingQueue(1);
+        /*ArrayBlockingQueue queue = new ArrayBlockingQueue(1);
         queue.add(new Object());
-        queue.take();
+        queue.take();*/
+
+        LinkedBlockingDeque linkedBlockingDeque = new LinkedBlockingDeque(1);
+        linkedBlockingDeque.add(1);   //添加失败 抛异常
+        linkedBlockingDeque.offer(1);//添加失败返回值
+        linkedBlockingDeque.offer(1,100, TimeUnit.MINUTES);
+        linkedBlockingDeque.put(1);//一直阻塞
+
+        linkedBlockingDeque.take();
     }
 }
