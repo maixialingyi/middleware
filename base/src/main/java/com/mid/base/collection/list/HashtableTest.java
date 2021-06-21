7f02a3a159df6903a59dc4a1f0eaf248c9645352
jiangshaoyue <jiangshaoyue@meituan.com>
2021-06-21 21:09:14 +0800

    w

---
+++ base/src/main/java/com/mid/base/collection/list/HashtableTest.java
@@ -0,0 +1,11 @@
+package com.mid.base.collection.list;
+
+import java.util.Hashtable;
+
+public class HashtableTest {
+
+    public static void main(String[] args) {
+        Hashtable hashtable = new Hashtable();
+        hashtable.put("","");
+    }
+}
